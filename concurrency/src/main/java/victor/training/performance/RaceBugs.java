635c4d48df6be24a1c0f9e4edb14cf325b3ce1ec
rafi.isaharov <rafi.isaharov@dh.com>
2024-11-05 16:37:57 +0200

    Day 2: best way to do multi-thread design: return rather than change data -with for of a parts

--- concurrency/src/main/java/victor/training/performance/RaceBugs.java
+++ concurrency/src/main/java/victor/training/performance/RaceBugs.java
@@ -52,32 +52,31 @@ public class RaceBugs {
     List<List<Integer>> parts = splitList(fullList, 2);
 
     ExecutorService pool = Executors.newCachedThreadPool();
-//    List<Future<Integer>> futureList = new ArrayList<>();
-//    for (List<Integer> part : parts) {
-//      Future<Integer> localTotalFuture = pool.submit(new Callable<Integer>() {
+    List<Future<Integer>> futureList = new ArrayList<>();
+    for (List<Integer> part : parts) {
+      Future<Integer> localTotalFuture = pool.submit(() -> countEven(part)); // java 8 style
+//        Future<Integer> localTotalFuture = pool.submit(new Callable<Integer>() {
 //        @Override
 //        public Integer call() throws Exception {
 //          return countEven(part);// this method is executed by a worker thread
 //        }
 //      });
-////      Future<Integer> localTotalFuture = pool.submit(() -> countEven(part)); // java 8 style
-//
-//      futureList.add(localTotalFuture);
-//    }
-
-    Future<Integer> firstHalfResultsFuture = pool.submit(() -> countEven(parts.get(0)));
-    Future<Integer> secondHalfResultsFuture = pool.submit(() -> countEven(parts.get(1)));
-    // 2 threads are now counting my even numbers
+    futureList.add(localTotalFuture);
+  }
     int total = 0;
-    total += firstHalfResultsFuture.get(); // blocks the main thread until the results of the first half are ready
-    total += secondHalfResultsFuture.get();
+
+//    Future<Integer> firstHalfResultsFuture = pool.submit(() -> countEven(parts.get(0)));
+//    Future<Integer> secondHalfResultsFuture = pool.submit(() -> countEven(parts.get(1)));
+//    // 2 threads are now counting my even numbers
+//    total += firstHalfResultsFuture.get(); // blocks the main thread until the results of the first half are ready
+//    total += secondHalfResultsFuture.get();
 
     pool.shutdown();
     pool.awaitTermination(1, MINUTES);
 
-//    for (Future<Integer> future : futureList) {
-//      total += future.get();
-//    }
+    for (Future<Integer> future : futureList) {
+      total += future.get();// blocks the main thread until the results of the first half are ready
+    }
     log.debug("Counted: " + total);
     log.debug("List.size: " + evenNumbers.size());
   }
